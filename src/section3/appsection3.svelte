6db97137cd34a50ebde20b0c9ef7644d4107647a
ju4ncito <villarrealjm2@gmail.com>
2022-09-25 20:54:37 -0300

    Llimpieza para section 4, instalado routeo npm

---
+++ src/section3/appsection3.svelte
@@ -0,0 +1,100 @@
+<script>
+  import Header from "./section3/design/Header.svelte";
+  import CardGrid from "./section3/posts/CardGrid.svelte";
+  import InputCustom from "./section3/design/InputCustom.svelte";
+  import Jumbotron from "./section3/design/Jumbotron.svelte";
+  const color = "light";
+  let titulo = "";
+  let imagen = "";
+  let descripcion = "";
+  let post = [
+    {
+      id: "1",
+      titulo: "Trabajando con svelte",
+      descripcion: "Realizando un curso de svelte ",
+      imagen:
+        "https://cdn.pixabay.com/photo/2017/04/25/22/28/despaired-2261021_960_720.jpg",
+    },
+    {
+      id: "2",
+      titulo: "Trabajando con vue",
+      descripcion: "Realizando un curso de vue ",
+      imagen:
+        "https://cdn.pixabay.com/photo/2016/11/23/14/41/action-figure-1853285_960_720.jpg",
+    },
+    {
+      id: "3",
+      titulo: "Trabajando con qng",
+      descripcion: "Realizando un curso de qng ",
+      imagen:
+        "https://cdn.pixabay.com/photo/2016/11/29/03/17/action-1867014_960_720.jpg",
+    },
+  ];
+
+  function agregarPost() {
+    const nuevoPost = {
+      id: Math.random().toString(),
+      titulo: titulo,
+      descripcion: descripcion,
+      imagen: imagen,
+    };
+
+    post = [nuevoPost, ...post]; ///spread operator, convierte un array en serie de argumentos injectable
+  }
+</script>
+
+<Header {color} />
+
+<div class="container">
+  <Jumbotron nombre="miscomponentes" let:mostrar>
+    <span slot="subtitulo">holaaa</span>
+    <p slot="parrafo">PARRAFO</p>
+
+    <div class:mostrar>
+      {#if mostrar}
+        <hr />
+        <button class="btn btn-danger"> btono</button>
+      {:else}
+        <h2>coloca el mouse encima</h2>
+      {/if}
+    </div>
+  </Jumbotron>
+
+  <CardGrid {post} />
+
+  <form on:submit|preventDefault={agregarPost}>
+    <!-- <input type="text" placeholder="Titulo" bind:value={titulo} />
+    <input type="text" placeholder="Titulo" bind:value={descripcion} />
+    <input type="text" placeholder="Titulo" bind:value={imagen} />
+    <button type="submit" class="btn btn-info">Guardar</button> -->
+
+    <InputCustom
+      type="text"
+      nombre="Titulo"
+      id="titulo"
+      placeholder="Titulo"
+      value={titulo}
+      on:input={(event) => (titulo = event.target.value)}
+    />
+
+    <InputCustom
+      type="text"
+      nombre="Imagen"
+      id="imagen"
+      placeholder="Imagen"
+      value={imagen}
+      on:input={(event) => (imagen = event.target.value)}
+    />
+
+    <InputCustom
+      control="textarea"
+      nombre="Descripcion"
+      id="descripcion"
+      placeholder="Descripcion"
+      value={descripcion}
+      on:input={(event) => (descripcion = event.target.value)}
+    />
+
+    <button type="submit" class="btn btn-info">Guardar</button>
+  </form>
+</div>
