4e85d18d4a8aab7f1c426e2bd919512c24e1362b
ju4ncito <villarrealjm2@gmail.com>
2022-10-08 19:37:06 -0300

    inicio card y grid

---
+++ src/Componentes/Card.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let title;
+  export let image;
+</script>
+
+<div class="card mb-3" style="width:18res;">
+  <img
+    src="https://image.tmdb.org/t/p/w500{image}"
+    alt={title}
+    width="100%"
+    height="500"
+  />
+
+  <div class="card-body">
+    <h5 class="card-title">{title}</h5>
+    <button class="btn btn-primary"> MORE</button>
+  </div>
+</div>
